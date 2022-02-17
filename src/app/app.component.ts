1d36e1332d6c6a3f998a97677272c3b9fb6e592a
lauradillonar <laura.dillon.ar@gmail.com>
2022-02-17 10:41:46 -0300

    Directivas en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular008-directivas';
+  armaduras = [
+    'Mark I',
+    'Mark II',
+    'Mark III',
+    'Mark IV',
+    'Mark V',
+    'Mark VI',
+    'Mark XLII'
+  ];
+  ironman = 'Tony Stark';
+  revelar_identidad = false; // cambiar por true
+  usar = this.armaduras[2];
+  superhero = 'Ironman';
 }
